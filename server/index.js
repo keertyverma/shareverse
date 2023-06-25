f1a5fb1868dc793476a79dd1ce16cdb51d6d6cda
Keerty Verma <keertyverma1303@gmail.com>
2023-06-25 20:09:03 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server is listening on PORT:${PORT}`));
